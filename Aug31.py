6d1c10dcc3e488804437de85a9a7bcd56a53adeb
ImportRandom0 <119878367+ImportRandom0@users.noreply.github.com>
2023-05-26 23:19:16 -0500

    Aug31

---
+++ Aug31.py
@@ -0,0 +1,10 @@
+print("It's good \n to be curious \n about many things. \n -Mr Rodgers")
+
+
+print("\"I'll be back.\" - Terminator")
+
+
+print("/////////////////////// \n || Victory is mine! || \n\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
+print("   \\/ \n  \\\\//\n \\\\\///\n ///\\\\\\\n  //\\\\\n   /\\")
+
+
