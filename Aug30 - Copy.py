0d5d982efc4f08248bc4ddcf14c80d7998f184c3
ImportRandom0 <119878367+ImportRandom0@users.noreply.github.com>
2023-05-26 23:19:04 -0500

    Aug30

---
+++ Aug30 - Copy.py
@@ -0,0 +1,33 @@
+def space():
+	print("")
+
+
+
+print("There's one thing that every coder must understand: ", "The print command.", " ", "There's one thing that every coder must understand: ", "The print command.", sep="\n")
+ 
+space() # this is a divider
+ 
+dessert = "Chocolate Ice-Cream"
+
+print(dessert)
+
+space() # this is a divider
+
+concearned = "Geometry"
+
+print(concearned)
+
+space() # this is a divider
+
+print(concearned, dessert)
+
+space() # this is a divider
+
+print("hello \nworld")
+print("i\tam\thungry")
+
+space() # this is a divider
+
+print(" 90\tpercent\tof\tgamblers\tquit\tbefore\tthey\thit\tit\tbig")
+
+space() # this is a divider
