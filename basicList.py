f333a78afee77e5a5f355c67cc1b4c699b3d9a39
ImportRandom0 <119878367+ImportRandom0@users.noreply.github.com>
2023-06-07 01:09:46 -0500

    BasicList.py

---
+++ basicList.py
@@ -0,0 +1,18 @@
+#Charlie Hudson
+#Peroid 6
+
+data = [27, 51, 33, -1, 101]
+print(data)
+data.append(15)
+data.append(61)
+data.append(74)
+data.append(12)
+data.append(12546)
+print(data)
+
+data2 = ["It", "was", "a", "stormy", "night"]
+data2.insert(3, "dark")
+data2.insert(4, "and")
+print(data2)
+data2[1] = "Is"
+print(data2)
