f3a25f0bef4f92ba1bd5440fc3eee4f1de8b4bc4
ImportRandom0 <119878367+ImportRandom0@users.noreply.github.com>
2023-06-10 22:05:21 -0500

    Add files via upload

---
+++ Compliments.py
@@ -0,0 +1,13 @@
+#Charlie Hudson
+#Peroid 6
+
+Name = input("What is your name?")
+
+if (Name == "Ant" or Name == "ANT" or Name == "Anthony" or Name == "ANTHONY"):
+	print("you have nice shoes")
+elif (Name == "Jonathan" or Name == "JONATHAN" or Name == "Jon" or Name == "JON"):
+	print("I like your hair")
+elif  (Name == "Garcia" or Name == "GARCIA" or Name == "Alejandro" or Name == "ALEJANDRO"):
+	print("I like your shirt")
+else:
+	print("I do not know your name")
